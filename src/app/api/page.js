19fbdc74a0d029e1cee8dd83edce644872587dd1
Subham Singh <geekysubham@gmail.com>
2023-09-08 05:02:50 +0530

    add dynamic routes and fetch data via nextjs

---
+++ src/app/api/page.js
@@ -0,0 +1,7 @@
+export default function BlogPostDetail(params) {
+    console.log(params)
+    return <main>
+        <h1>Hello Detail view</h1>
+    </main>
+}
+ 
---
+++ src/app/api/posts/route.js
@@ -0,0 +1,18 @@
+
+import { NextResponse } from "next/server";
+
+
+export async function GET() {
+    return NextResponse.json(
+        {
+            items: [
+                 {id: 1, title: "Hello World"},
+            ]
+        })
+}
+
+
+// export async function POST(){
+//     return NextResponse.json({hello: "POST"})
+    
+// }
