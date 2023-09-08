ade55c9332a7ef8bfc5023d2f5adb950f74dd787
Subham Singh <geekysubham@gmail.com>
2023-09-08 06:37:32 +0530

    update fetch to revalidate every 10 sec

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,7 +4,7 @@ async function getData(){
     //1 endpoint - API
     const domain = getDomain()
     const endpoint = `${domain}/api/posts` // -> third party api request
-    const res = await fetch(endpoint) // http GET method
+    const res = await fetch(endpoint, {next: {revalidate: 10}}) // http GET method
 
     if (!res.ok){
         throw new Error("Failed to fetch data")
